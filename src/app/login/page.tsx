fc677d25f8fb733eebb7d5e4920dde0712496f3d
Mystik <mustafa.budak@fom-net.de>
2024-07-31 02:14:05 +0200

    name change

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -43,7 +43,7 @@ export default function Login() {
     return (
         <div className="max-w-md w-full mx-auto rounded-none md:rounded-2xl p-4 md:p-8 shadow-input bg-white dark:bg-black">
             <h2 className="font-bold text-xl text-neutral-800 dark:text-neutral-200">
-                Welcome to my TimeTracker
+                Welcome to notbooking.com
             </h2>
             <p className="text-neutral-600 text-sm max-w-sm mt-2 dark:text-neutral-300">
             </p>
